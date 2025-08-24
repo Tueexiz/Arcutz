8f3b13eb8617bcd0c9e627e97324e4764863300d
Tueexiz <robin.demesmay@gmail.com>
2025-08-25 01:19:20 +0200

    Fix CSS imports with relative paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css';
-import '@/styles/carousel.css'; // ✅ import global CSS here
+import '../styles/globals.css'
+import '../styles/carousel.css'
+ // ✅ import global CSS here
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
